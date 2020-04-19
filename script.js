7181a8d5938ec06212d7dd352e27e64ee329e426
Praise Obia <praizobia@gmail.com>
2020-04-19 23:54:26 +0100

    Js Task3

---
+++ script.js
@@ -0,0 +1,77 @@
+let num = [
+    i = "1","2", "3","4","5","6","7", "8", "9","10",
+
+]
+
+function divisible(num) { 
+let  div = []; 
+
+for ( let i = 1 ; i <= 10, i++)
+{
+if (i%2 === 0) {
+ console.log (yu)  
+} else
+if ( i%3 === 0) {
+  console.log (gi)
+} else
+if (i%5 === 0 ) {
+   console.log (oh) 
+} else
+if (i%2 === 0 && i%3 === 0)
+   console.log(yu-gi)
+} else
+if (i%2 == 0 && i%5 == 0) {
+    console.log (yu-oh)
+} else
+if (i%3 == 0 && I%5 == 0) {
+    console.log (gi-oh)
+} else
+if (i%2 == 0 && i%3 == 0 && i%5 == 0) {
+    console.log(yu-gi-oh)
+}
+
+};
+
+ console.log (div);
+ return(div)
+
+
+divisible(num)
+divisible(100); 
+divisible(30);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
